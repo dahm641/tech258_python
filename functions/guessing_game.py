aaa2b3907e798af80c3d37919b75e8728369348a
dahm641 <danial646@hotmail.co.uk>
2024-04-20 22:36:48 +0100

    added error handling rule if not an integer

--- functions/guessing_game.py
+++ functions/guessing_game.py
@@ -10,27 +10,32 @@ def guessing_game():
     while game_active:
 
         print(start_message)
-        user_guess = int(input())
+        user_guess = input()
+        try:
+            int(user_guess)
+        except:
+            print(f"{user_guess} is not an integer! Try again")
+        else:
+            user_guess = int(user_guess)
+            if user_guess < game_number:
+                print('too low, guess again!')
 
-        if user_guess < game_number:
-            print('too low, guess again!')
-
-        elif user_guess > game_number:
-            print('too high! guess again')
+            elif user_guess > game_number:
+                print('too high! guess again')
 
 
-        elif (user_guess) == (game_number):
-            print('Correct!')
-            user_input = False
-            while user_input is False:
-                user_answer = input("Would you like to play again? y/n")
-                if str(user_answer) == "y":
-                    user_input = True
-                    game_active = True
-                    game_number = int(random.randint(1, 100))
-                elif str(user_answer) == "n":
-                    game_active = False
-                    user_input = True
-                else: print(f"{user_answer} is not a valid input")
+            elif (user_guess) == (game_number):
+                print('Correct!')
+                user_input = False
+                while user_input is False:
+                    user_answer = input("Would you like to play again? y/n")
+                    if str(user_answer) == "y":
+                        user_input = True
+                        game_active = True
+                        game_number = int(random.randint(1, 100))
+                    elif str(user_answer) == "n":
+                        game_active = False
+                        user_input = True
+                    else: print(f"{user_answer} is not a valid input")
 
 guessing_game()
